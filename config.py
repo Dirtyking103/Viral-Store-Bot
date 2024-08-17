c308f4c83a8e2526517d587e6efb0dd76b99ed74
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-08-17 11:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://dirtyking102.blogspot.com/2024/08/dirtyking.html") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://dirtyking102.blogspot.com/2024/08/dirtyking.html") 
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
@@ -75,5 +75,5 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://aniflixofficials.koyeb.app")
     
